f2b49f3279e0f37af8348b30d5feeb6fb2c5442c
Deepak7009 <deepak7009verma@gmail.com>
2024-05-18 11:33:36 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import './App.css';
-import ProductInfo from './components/SectionOne';
-import './Style/Section3.css';
-import Section3 from './components/Section3';
-import Foot from './components/Foot';
-import Section2 from './components/Section2';
-
+import "./App.css";
+import ProductInfo from "./components/SectionOne";
+import "./Style/Section3.css";
+import Section3 from "./components/Section3";
+import Foot from "./components/Foot";
+import Section2 from "./components/Section2";
 
 function App() {
   return (
     <div className="App">
-    <ProductInfo />
-    <Section2/>
+      <ProductInfo />
+      <Section2 />
       <Section3 />
       <Foot />
     </div>
-
   );
 }
 
--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -44,80 +44,139 @@ const Section = () => {
   };
 
   const buttonClass =
-    "w-full flex justify-end py-2 mb-2 text-white rounded-lg text-lg";
+    "w-full flex justify-end py-2 mb-0 text-white text-lg";
 
   return (
     <>
-    <ProductFeatures/>
-    <div className="container flex justify-center mt-4 mx-auto px-4">
-      <div className=" w-[70%] flex flex-wrap">
-        <div className="w-full md:w-1/4 pt-4 bg-customBgColorHex">
-          <div className="bg-customBgColorHex p-4 rounded-lg">
-            <h2 className="text-xl font-bold mb-4 float-right">DISCOVER</h2>
-            <div className="img">
-              <img
-                src={logo}
-                alt="logo"
-                className="w-full h-auto max-w-xs mx-auto my-4"
-              />
-            </div>
-            <div className="buttons ml-4 ">
-              <div
-                className={`${buttonClass} ${
-                  active === "Product" ? "bg-customBgColorHSL" : ""
-                }`}
-                onClick={() => handleClick("Product")}
-              >
-                <p className="text-lg text-white" style={{ fontSize: "25px" }}>
-                  Product
-                </p>
+      <ProductFeatures />
+      <div className="container flex justify-center mt-4 mx-auto px-4">
+        <div className=" flex flex-wrap">
+          <div className="w-full md:w-1/4 pt-4 bg-customBgColorHex">
+            <div className="bg-customBgColorHex p-4 rounded-lg">
+              <h2 className="text-xl font-bold mb-4 float-right">DISCOVER</h2>
+              <div className="img">
+                <img
+                  src={logo}
+                  alt="logo"
+                  className="w-full h-auto max-w-xs mx-auto my-4"
+                />
               </div>
-              <hr className="my-2" />
-              <div
-                className={`${buttonClass} ${
-                  active === "Benefits" ? "bg-customBgColorHSL" : ""
-                }`}
-                onClick={() => handleClick("Benefits")}
-              >
-                <p className="text-lg" style={{ fontSize: "25px" }}>
-                  Benefits
-                </p>
-              </div>
-              <hr className="my-2" />
-              <div
-                className={`${buttonClass} ${
-                  active === "Our Guarantee" ? "bg-customBgColorHSL" : ""
-                }`}
-                onClick={() => handleClick("Our Guarantee")}
-              >
-                <p className="text-lg" style={{ fontSize: "25px" }}>
-                  Our Guarantee
-                </p>
-              </div>
-              <hr className="my-2" />
-              <div
-                className={`${buttonClass} ${
-                  active === "About FitSpresso" ? "bg-customBgColorHSL" : ""
-                }`}
-                onClick={() => handleClick("About FitSpresso")}
-              >
-                <p className="text-lg" style={{ fontSize: "25px" }}>
-                  About FitSpresso
-                </p>
+              <div className="buttons ml-10 ">
+                <div
+                  className={`${buttonClass} ${
+                    active === "Product" ? "bg-customBgColorHSL" : ""
+                  }`}
+                  onClick={() => handleClick("Product")}
+                >
+                  <p
+                    className="text-lg text-white"
+                    style={{ fontSize: "25px" }}
+                  >
+                    Product
+                  </p>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6 "
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                    />
+                  </svg>
+                </div>
+                <hr className="my-0 bg-customBgColorHSL h-0.5" />
+                <div
+                  className={`${buttonClass} ${
+                    active === "Benefits" ? "bg-customBgColorHSL" : ""
+                  }`}
+                  onClick={() => handleClick("Benefits")}
+                >
+                  <p className="text-lg" style={{ fontSize: "25px" }}>
+                    Benefits
+                  </p>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6 "
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                    />
+                  </svg>
+                </div>
+                <hr className="my-0 bg-customBgColorHSL h-0.5" />
+                <div
+                  className={`${buttonClass} ${
+                    active === "Our Guarantee" ? "bg-customBgColorHSL" : ""
+                  }`}
+                  onClick={() => handleClick("Our Guarantee")}
+                >
+                  <p className="text-lg" style={{ fontSize: "25px" }}>
+                    Our Guarantee
+                  </p>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6 "
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                    />
+                  </svg>
+                </div>
+                <hr className="my-0 bg-customBgColorHSL h-0.5" />
+                <div
+                  className={`${buttonClass} ${
+                    active === "About FitSpresso" ? "bg-customBgColorHSL" : ""
+                  }`}
+                  onClick={() => handleClick("About FitSpresso")}
+                >
+                  <p className="text-lg" style={{ fontSize: "25px" }}>
+                    About FitSpresso
+                  </p>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6 "
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="w-full md:w-3/4 pt-4 bg-customBgColor">
-        <div className="bg-customBgColor p-4 rounded-lg">
-            {active === "Product" && <Product />}
-            {active === "Benefits" && <Benefits />}
-            {active === "Our Guarantee" && <Guarantee />}
-            {active === "About FitSpresso" && <FitSpresso />}
+          <div className="w-full md:w-3/4 pt-4 bg-customBgColor">
+            <div className="bg-customBgColor p-4 rounded-lg">
+              {active === "Product" && <Product />}
+              {active === "Benefits" && <Benefits />}
+              {active === "Our Guarantee" && <Guarantee />}
+              {active === "About FitSpresso" && <FitSpresso />}
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
